f6a8a036c03e8a5719f99c6372680ad578264aeb
Antoine Rousseau <_antoine_@metalu.net>
2021-11-24 18:50:57 +0100

    add Trampo project

---
+++ Trampo/trampo_fw/main.c
@@ -0,0 +1,109 @@
+/*********************************************************************
+ *               analog example for Versa2.0
+ *	Analog capture on connector K1
+ *********************************************************************/
+
+#define BOARD Versa2
+
+#include <fruit.h>
+#include <analog.h>
+
+t_delay mainDelay;
+unsigned int threshold = 120;
+unsigned char presence = 0;
+unsigned char triggered = 0;
+unsigned int maximum;
+t_delay maxDelay;
+unsigned int millisCount = 0;
+#define WINDOW_MS 100
+#define RESTART_MS 500
+
+void setup(void) {	
+//----------- Setup ----------------
+	fruitInit();
+			
+	pinModeDigitalOut(LED); 	// set the LED pin mode to digital out
+	digitalClear(LED);		// clear the LED
+	delayStart(mainDelay, 5000); 	// init the mainDelay to 5 ms
+
+//----------- Analog setup ----------------
+	analogInit();		// init analog module
+	
+	analogSelect(0,K1);	// assign connector K1 to analog channel 0
+}
+
+void trampoService()
+{
+	unsigned int current = analogGet(0);
+	unsigned char tmp_presence = analogGet(0) > threshold;
+	
+	if(presence == 0) {
+		if(tmp_presence == 0) return;
+		maximum = current;
+		presence = 1;
+		triggered = 1;
+		millisCount = 0;
+	} else {
+		if(triggered) {
+			if(current > maximum) maximum = current;
+			if(millisCount > WINDOW_MS) {
+				printf("CT %d\n", maximum);
+				triggered = 0;
+			}
+			return;
+		}
+		if(tmp_presence) millisCount = 0;
+		else {
+			if(millisCount > RESTART_MS) presence = 0;
+		}
+	}
+}
+
+unsigned char analogSendCount;
+void loop() {
+// ---------- Main loop ------------
+	fraiseService();	// listen to Fraise events
+	analogService();	// analog management routine
+	trampoService();
+	
+	if(delayFinished(mainDelay)) // when mainDelay triggers :
+	{
+		delayStart(mainDelay, 5000); 	// re-init mainDelay
+		if(analogSendCount++ > 20) {
+			analogSend();		// send analog channels that changed
+			analogSendCount = 0;
+		}
+		millisCount += 5;
+	}
+}
+
+// Receiving
+
+void fraiseReceiveChar() // receive text
+{
+	unsigned char c;
+	
+	c=fraiseGetChar();
+	if(c=='L'){		//switch LED on/off 
+		c=fraiseGetChar();
+		digitalWrite(LED, c!='0');		
+	}
+	else if(c=='E') { 	// echo text (send it back to host)
+		printf("C");
+		c = fraiseGetLen(); 			// get length of current packet
+		while(c--) printf("%c",fraiseGetChar());// send each received byte
+		putchar('\n');				// end of line
+	}	
+}
+
+void fraiseReceive()
+{
+	unsigned char c = fraiseGetChar();
+	
+	if(c == 1) printf("CC'etait 1\n");
+	else if(c == 2) {
+		long int l;
+		l = fraiseGetLong();
+		printf("CLa c'est 2 avec: %ld\n", l);
+	}
+}
