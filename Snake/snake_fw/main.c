0f3938a0bae71e8c48205efa5c94cb88515ff92c
Antoine Rousseau <_antoine_@metalu.net>
2021-11-26 17:32:46 +0100

    add Snake project

---
+++ Snake/snake_fw/main.c
@@ -0,0 +1,71 @@
+/*********************************************************************
+ *               analog example for Versa2.0
+ *	Analog capture on connector K1
+ *********************************************************************/
+
+#define BOARD Versa2
+
+#include <fruit.h>
+#include <analog.h>
+
+t_delay mainDelay;
+
+void setup(void) {	
+//----------- Setup ----------------
+	fruitInit();
+			
+	pinModeDigitalOut(LED); 	// set the LED pin mode to digital out
+	digitalClear(LED);		// clear the LED
+	delayStart(mainDelay, 5000); 	// init the mainDelay to 5 ms
+
+//----------- Analog setup ----------------
+	analogInit();		// init analog module
+	
+	analogSelect(0,K1);	// assign connector K1 to analog channel 0
+	analogSelect(1,K2);
+	analogSelect(2,K3);
+	analogSelect(3,K4);
+}
+
+unsigned char analogSendCount;
+void loop() {
+// ---------- Main loop ------------
+	fraiseService();	// listen to Fraise events
+	analogService();	// analog management routine
+	
+	if(delayFinished(mainDelay)) // when mainDelay triggers :
+	{
+		delayStart(mainDelay, 5000); 	// re-init mainDelay
+		if(analogSendCount++ > 10) {
+			analogSend();		// send analog channels that changed
+			analogSendCount = 0;
+		}
+	}
+}
+
+// Receiving
+
+void fraiseReceiveChar() // receive text
+{
+	unsigned char c;
+	
+	c=fraiseGetChar();
+	if(c=='L'){		//switch LED on/off 
+		c=fraiseGetChar();
+		digitalWrite(LED, c!='0');		
+	}
+	else if(c=='E') { 	// echo text (send it back to host)
+		printf("C");
+		c = fraiseGetLen(); 			// get length of current packet
+		while(c--) printf("%c",fraiseGetChar());// send each received byte
+		putchar('\n');				// end of line
+	}	
+}
+
+void fraiseReceive()
+{
+	unsigned char c = fraiseGetChar();
+	
+	switch(c) {
+	}
+}
